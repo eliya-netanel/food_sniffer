9463c0ddde3e3a238f46891b3352f62e5eb0100e
Pinchas Hodadad <pinchas.hodadad@gmail.com>
2018-08-20 12:01:43 +0300

    added seeds and changed user model with has_many scans

--- app/models/scan.rb
+++ app/models/scan.rb
@@ -1,5 +1,6 @@
 class Scan < ApplicationRecord
   belongs_to :user
   belongs_to :food_item
-  # validates :result ,optional: true #, presence: true,, inclusion: { in: [ "true", "false", "nil"] }
+
 end
+ # validates :result ,optional: true #, presence: true,, inclusion: { in: [ "true", "false", "nil"] }
--- db/migrate/20180819142505_create_scans.rb
+++ db/migrate/20180819142505_create_scans.rb
@@ -3,7 +3,7 @@ class CreateScans < ActiveRecord::Migration[5.2]
     create_table :scans do |t|
       t.references :user, foreign_key: true
       t.references :food_item, foreign_key: true
-      t.string :date
+      t.date :date
       t.boolean :result
 
       t.timestamps
