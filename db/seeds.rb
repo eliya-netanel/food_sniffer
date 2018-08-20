fe01be4e91330bcbce6a535459c91c33cdcd0fe8
Pinchas Hodadad <pinchas.hodadad@gmail.com>
2018-08-20 15:17:08 +0300

    added changes to date:string to date:date.today and changed type to type_of in ingredients and food_item

--- db/seeds.rb
+++ db/seeds.rb
@@ -7,10 +7,10 @@ Scan.destroy_all
 user_1 = User.create!(user_name:"Pini", first_name:"pinchas", last_name:"hodadad", email:"pinchas.hodadad@gmail.com",password:1234567)
 
 #food_items
-food_items_1 = FoodItem.create!(name:"nutella")
+food_items_1 = FoodItem.create!(name:"nutella", type_of: "sweet")
 
 # #ingredients_
-ingredients_1 = Ingredient.create!(name:"sugar")
+ingredients_1 = Ingredient.create!(name:"sugar", type_of: "condiment")
 
 # #scans
 scan_1 = Scan.create!(date:Date.today, result: true, user_id: user_1.id, food_item_id: food_items_1.id)
