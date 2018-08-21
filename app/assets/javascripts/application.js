30cfdd1722838eebdc7030c6799ebac8a2012024
ELIYA NETANEL <eliya313@gmail.com>
2018-08-21 18:01:31 +0300

    we implemented the barcode in scanner new

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -12,9 +12,11 @@
 //
 //
 //= require jquery
+//= require jquery-ujs
 //= require turbolinks
 //= require bootstrap-sprockets
 //= require activestorage
+//= require rails-ujs
 //= require_tree .
 
 
@@ -30,74 +32,3 @@
 
 
 
-import 'bootstrap';
-
-function order_by_occurrence(arr) {
-  var counts = {};
-  arr.forEach(function(value){
-      if(!counts[value]) {
-          counts[value] = 0;
-      }
-      counts[value]++;
-  });
-
-  return Object.keys(counts).sort(function(curKey,nextKey) {
-      return counts[curKey] < counts[nextKey];
-  });
-}
-
-function load_quagga(){
-  if ($('#barcode-scanner').length > 0 && navigator.mediaDevices && typeof navigator.mediaDevices.getUserMedia === 'function') {
-
-    var last_result = [];
-    if (Quagga.initialized == undefined) {
-      // registers a callback function which is triggered whenever a barcode pattern has been located and decoded successfully
-      // the data object contins info about the decoding process including the detected code.
-      // which is obtained by calling data.codeResult.code
-      Quagga.onDetected(function(result) {
-        var last_code = result.codeResult.code;
-        last_result.push(last_code);
-        if (last_result.length > 20) {
-          code = order_by_occurrence(last_result)[0];
-          last_result = [];
-          // if the decoder is running after calling stop the decoder does not process any more images.
-          // if the camerstream was requested on initialization this disconnects the camera.
-          Quagga.stop();
-          $.ajax({
-            type: "POST",
-            url: '/scans/new',
-            data: { upc: code }
-          });
-        }
-      });
-    }
-
-    // This method initialises the library fro our given config
-    // It invokes the function err when Quagga has finsihed its bootstrapping phase
-    // I t also requests camera access if real-time detection is is configured
-    // Note: if the inputSteam.type is set to LiveStream bu tthe browser doesn't support
-    // the API this wuld cause an exception to be thrown. Likewise
-    // the user denying access to the camera.
-    Quagga.init({
-      // defines sources of images within QuaggaJS
-      inputStream : {
-        name : "Live",
-        type : "LiveStream",
-        numOfWorkers: navigator.hardwareConcurrency,
-        // target can be a string e.g. a css selector matching a DOM node or a DOM node
-
-        target: document.querySelector('#barcode-scanner')
-      },
-      decoder: {
-          readers : ['ean_reader','ean_8_reader','code_39_reader','code_39_vin_reader','codabar_reader','upc_reader','upc_e_reader']
-      }
-    },function(err) {
-        if (err) { console.log(err); return }
-        Quagga.initialized = true;
-        // The start method starts the video stream and begins locating and deconding images.
-        Quagga.start();
-    });
-
-  }
-};
-$(document).on('turbolinks:load', load_quagga);
--- app/controllers/scans_controller.rb
+++ app/controllers/scans_controller.rb
@@ -1,4 +1,6 @@
 class ScansController < ApplicationController
+   # skip_before_action :verify_authenticity_token
+  # skip_before_action :authenticate_user!#, only: [:home]
   def new
     @scan = Scan.new
     # @food_item = FoodIten.new
@@ -6,13 +8,15 @@ class ScansController < ApplicationController
   end
 
   def get_barcode
-    @food_item = FoodIten.find_or_initialize_by(upc: params[:upc])
-    unless @product.new.record?
-      redirect_to scan_path(@food_item)
-    end
+
+    # @food_item = FoodIten.find_or_initialize_by(upc: params[:upc])
+    # unless @product.new.record?
+      # redirect_to scan_path(@food_item)
+    # end
   end
 
   def create
+    redirect_to root_path
     @scan = Scan.new
     @scan.user = current_user
     @scan.result = call_to_api
