4864d8d085c3526e29e4ecb30494f545ce3ebe63
Pinchas Hodadad <pinchas.hodadad@gmail.com>
2018-08-22 15:53:38 +0300

    added index action to diet controller

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   devise_for :users
   root to: 'pages#home'
 
-  resources :diets, only: [:new, :create, :edit, :update]
+  resources :diets, only: [:show, :new, :create, :edit, :update]
   resources :scans, only: [:new, :create, :show, :get_barcode]
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
