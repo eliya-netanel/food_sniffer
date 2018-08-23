9dd8d0d9614229e0a35d1857d495c28bd7e4b765
Pinchas Hodadad <pinchas.hodadad@gmail.com>
2018-08-23 19:52:06 +0300

    added logic to the diet controller to show all vegan ingredients once clicked on it

--- app/models/diet.rb
+++ app/models/diet.rb
@@ -1,5 +1,5 @@
 class Diet < ApplicationRecord
   belongs_to :user, optional: true
   has_many :diet_ingredients
-  has_many :ingredients, through: :diet_ingredients
+  has_many :ingredients, through: :diet_ingredients, dependent: :destroy
 end
