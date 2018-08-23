686e342a76cd1e0df12ba2a14ae0f591aa43f385
Pinchas Hodadad <pinchas.hodadad@gmail.com>
2018-08-23 13:29:32 +0300

    created the seeds and modified accordingly for the seeds and started with the controller

--- app/models/diet.rb
+++ app/models/diet.rb
@@ -1,4 +1,5 @@
 class Diet < ApplicationRecord
-  belongs_to :user
-  has_many :ingredients
+  belongs_to :user, optional: true
+  has_many :diet_ingredients
+  has_many :ingredients, through: :diet_ingredients
 end
