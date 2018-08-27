25fb90bff046e695645ae99bd5e0929b89d97b95
Guy Pyetan <guypyetan@googlemail.com>
2018-08-27 14:39:00 +0300

    facebook login

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2018_08_26_100912) do
+ActiveRecord::Schema.define(version: 2018_08_27_081635) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -79,6 +79,10 @@ ActiveRecord::Schema.define(version: 2018_08_26_100912) do
     t.string "first_name"
     t.string "last_name"
     t.string "user_name"
+    t.string "provider"
+    t.string "uid"
+    t.string "token"
+    t.datetime "token_expiry"
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
   end
